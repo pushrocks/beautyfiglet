e295f471334c9e3d032e2fec4feb4b6e04108431
Phil Kunz <gitlab@philkunz.com>
2018-03-05 01:21:25 +0100

    update

---
+++ dist/beautyfiglet.plugins.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const removeme = {};
+exports.removeme = removeme;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYmVhdXR5ZmlnbGV0LnBsdWdpbnMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90cy9iZWF1dHlmaWdsZXQucGx1Z2lucy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLE1BQU0sUUFBUSxHQUFHLEVBQUUsQ0FBQztBQUVsQiw0QkFBUSJ9
---
+++ test/test.ts
@@ -0,0 +1,8 @@
+import { expect, tap } from 'tapbundle'
+import * as beautyfiglet from '../ts/index'
+
+tap.test('first test', async () => {
+  console.log(beautyfiglet.standardExport)
+})
+
+tap.start()
